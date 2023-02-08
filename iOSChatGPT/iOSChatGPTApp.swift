e72135769afbff06f54f8159160acdcc8cccffee
Dante Hargrow <dante.hargrow@vmlyr.com>
2023-02-07 21:40:28 -0600

    Initial Commit

---
+++ iOSChatGPT/iOSChatGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSChatGPTApp.swift
+//  iOSChatGPT
+//
+//  Created by Dante Hargrow on 2/7/23.
+//
+
+import SwiftUI
+
+@main
+struct iOSChatGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
